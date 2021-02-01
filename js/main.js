a7f649c8411633f77a3ce56efb8dca734be6dbed
Beka Maisuradze <bekamais@Bekas-MacBook-Pro.local>
2021-02-01 20:24:14 +0400

    inital commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+const ready = () => {
+   new fullpage('#fullpage').fullpage({
+       
+   })
+};
+
+document.addEventListener("DOMContentLoaded", ready);
+
+// class FullPageConfig {
+//   constructor() {
+//     this.initFullPage();
+//   }
+
+//   initFullPage() {
+
+//   }
+// }
